df3d15866c4682e82165b57cd99abede7368ac2d
padznich <slabkopg@gmail.com>
2017-02-08 11:46:10 +0300

    [TOX]

--- modules/summator.py
+++ modules/summator.py
@@ -7,5 +7,9 @@ def s1(x, y):
     '''For docs check'''
     return x + y
 
+def s2(x, y):
+    '''For docs check'''
+    return x - y
+
 if __name__ == "__main__":
-    print "Hello world .py"
+    print("Hello world .py")
--- tests/test_m_summator.py
+++ tests/test_m_summator.py
@@ -5,6 +5,9 @@
 from modules import summator
 
 
-def test_summator():
-
+def test_summator_s1():
     assert summator.s1(2, 3) == 5
+
+
+def test_summator_s2():
+    assert summator.s2(5, 4) == 1
